3db455c29d1318736f9b0bdc8d122394c09b440e
Zanko Yaroslav <arikzanko9@gmail.com>
2025-11-02 10:20:51 +0200

    ms

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
+import os
+os.remove("file.txt")
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    def __enter__(self) -> "CleanUpFile":
+        return self
+
+    def __exit__(self) -> None:
+        os.remove(f"{self.path}")
